3e052e324a291cc62809a845f9e931a3d2ab6973
Jihunz <thespringsong@gmail.com>
2022-03-18 00:48:22 +0900

    feat: main.js, main.css 추가

---
+++ src/main/webapp/resources/js/main.js
@@ -0,0 +1,14 @@
+// swiper 동작 설정을 위한 객체
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+    },
+    keyboard: {
+        enabled: true,
+      },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
