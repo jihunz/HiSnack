c5e3e84143e96b87d6123fc88f04dc8da6b14af6
Jihunz <thespringsong@gmail.com>
2022-03-22 21:43:19 +0900

    refactor: 공백, 주석 제거

--- src/main/webapp/resources/js/signup.js
+++ src/main/webapp/resources/js/signup.js
@@ -1,26 +1,26 @@
-$(function() {
-	$("#signupBtn").click((e) => {
+$(function () {
+    $("#signupBtn").click((e) => {
         chkNull(e);
         chkPwd(e);
     });
 
     $("#userId").on({
-        "keyup" : () => restrictId(),
-        "focusout" : () => chkId()
+        "keyup": () => restrictId(),
+        "focusout": () => chkId()
     });
 
     $("#password").change(() => pwdRegx());
 });
 
 function chkNull(e) {
-   const inputNum = $("form input").length
+    const inputNum = $("form input").length
 
-    for(let i=1; i<=inputNum; i++) {
+    for (let i = 1; i <= inputNum; i++) {
         let nthLabel = $(`form > div:nth-child(${i}) > label`);
         let nthInput = $(`form > div:nth-child(${i}) > input`);
-		let inputType = $(nthLabel).text();
+        let inputType = $(nthLabel).text();
 
-        if(typeof $(nthInput).val() == "undefined"|| $(nthInput).val() == null || $(nthInput).val() == "") {
+        if (typeof $(nthInput).val() == "undefined" || $(nthInput).val() == null || $(nthInput).val() == "") {
             e.preventDefault();
             alert(`${inputType}를(을) 입력해주세요`);
             nthInput.focus();
@@ -29,12 +29,11 @@ function chkNull(e) {
     }
 }
 
-// state로 chkNull, chkPwd 활용 여부 결정해야함
 function chkPwd(e) {
     const pwd = $("#password");
     const pwdConfirm = $("#pwdConfirm");
 
-    if(pwd.val() !== pwdConfirm.val()) {
+    if (pwd.val() !== pwdConfirm.val()) {
         e.preventDefault();
         alert("비밀번호가 일치하지 않습니다");
         pwd.val("").focus();
@@ -47,48 +46,47 @@ function pwdRegx() {
     const pwd = $("#password");
     const regx = /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
 
-	if (!regx.test(pwd.val())) {
+    if (!regx.test(pwd.val())) {
         // alert("비밀번호 형식을 확인해주세요 \n 비밀번호는 최소 8~15자, 하나 이상의 숫자와 문자, 특수기호를 포함해야합니다")
         pwd.val("").attr("placeholder", "비밀번호 형식을 확인해주세요");
         pwd.focus();
         return;
-	} else {
+    } else {
         pwd.attr("placeholder", "Password");
     }
 }
 
 function chkId() {
-	let userId = $("#userId").val();
+    let userId = $("#userId").val();
 
-    if(userId == "") return;
-	
-	$.ajax(`rest/member/${userId}`, {
-		method: "GET",
-		success: result => {
-			console.log(result);
-			idMsg(result);
+    if (userId == "") return;
+
+    $.ajax(`rest/member/${userId}`, {
+        method: "GET",
+        success: result => {
+            console.log(result);
+            idMsg(result);
             return;
-		},
-		error: xhr => {
-			idErrMsg(xhr);
-		}
-	})
-
-	function idErrMsg(xhr) {
-		alert(`아이디 중복확인 중 오류 발생: ${xhr.statusText}`);
-		$("#userId").val("");
-        return;
-	}
-
-	function idMsg(result) {
-		if (result.length < 1) {
-			alert("사용 가능한 아이디입니다");
+        },
+        error: xhr => {
+            idErrMsg(xhr);
         }
-		else {
-			alert("사용 중인 아이디입니다");
-			$("#userId").val("");
-		}
-	}
+    })
+
+    function idErrMsg(xhr) {
+        alert(`아이디 중복확인 중 오류 발생: ${xhr.statusText}`);
+        $("#userId").val("");
+        return;
+    }
+
+    function idMsg(result) {
+        if (result.length < 1) {
+            alert("사용 가능한 아이디입니다");
+        } else {
+            alert("사용 중인 아이디입니다");
+            $("#userId").val("");
+        }
+    }
 }
 
 // 특수문자와 공백에 대한 정규식으로
@@ -100,7 +98,7 @@ function restrictId() {
     const blank = /\s/g;
     const userId = $("#userId").val();
 
-    if(spacial.test(userId) || blank.test(userId)) {
+    if (spacial.test(userId) || blank.test(userId)) {
         alert("특수문자, 공백은 입력할 수 없습니다");
         $("#userId").val("");
         return;
