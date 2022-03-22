00b2e82a0ad62e95f64b336f4e0a3a40243c3f19
Jihunz <thespringsong@gmail.com>
2022-03-22 20:52:17 +0900

    fix: dropdown 출력 에러 수정

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -1,29 +1,29 @@
 $(function() {
-    showId();
+	showId();
 });
 
 // swiper 동작 설정을 위한 객체
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-    },
-    keyboard: {
-        enabled: true,
-      },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    }
+	pagination: {
+		slidesPerView: 3,
+		spaceBetween: 30,
+	},
+	keyboard: {
+		enabled: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	}
 });
 
+// 로그인을 하지 않았을 경우 dropdown 숨김,
+// 로그인 완료 시 loginBtn의 text를 id로 교체
 function showId() {
-    const loginBtn = $("#loginBtn");
-    
-    if(user.userId != '') {
-        loginBtn.removeAttr("onClick").text(`${user.userId} 님`);
-    } else {
-        loginBtn.attr("oncClick", "location.href='login'")
-        $(".dropdown-menu").css("display", "none");
-    }
+	const loginBtn = $("#loginBtn");
+	if (user.userId != '') {
+		loginBtn.removeAttr("onClick").text(`${user.userId} 님`);
+	} else {
+		loginBtn.attr("onClick", "location.href='login'").removeAttr("data-bs-toggle");
+	}
 }
