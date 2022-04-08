361a0d92ab9f7c18ed934d7791cb961696a52294
tivmzk <ost0108@naver.com>
2022-04-08 18:45:32 +0900

    refactor : 마이페이지에서 PathVariable 제거

--- src/main/java/kr/ac/hisnack/controller/MemberController.java
+++ src/main/java/kr/ac/hisnack/controller/MemberController.java
@@ -1,10 +1,13 @@
 package kr.ac.hisnack.controller;
 
-import org.springframework.stereotype.Controller;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.ac.hisnack.model.Member;
 
 /**
  * 마이 페이지 컨틀로러 
@@ -18,8 +21,12 @@ public class MemberController {
  * 회원의 마이페이지로 이동시키는 메소드
  * @param id : 회원의 id
  */
-	@GetMapping("/{id}")
-	public String mypage(@PathVariable String id) {
+	@GetMapping
+	public String mypage(HttpSession session, Model model) {
+		Member user = (Member) session.getAttribute("user");
+		
+		model.addAttribute("item", user);
+		
 		return "member";
 	}
 }
