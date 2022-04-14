884a054555bdd2480a38eae127410af9b7758daa
tivmzk <ost0108@naver.com>
2022-04-14 16:40:25 +0900

    feat : exerd review 수정, review view css 추가

--- src/main/webapp/WEB-INF/views/review/item.jsp
+++ src/main/webapp/WEB-INF/views/review/item.jsp
@@ -5,22 +5,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>리뷰 상세</title>
+	<jsp:include page="/WEB-INF/views/common/head.jsp"></jsp:include>
+	<link rel="stylesheet" href="/re/css/review.css" />
+	<script src="/re/js/review_rating.js"></script>
 </head>
 <body>
-	<div>
-		<h1>리뷰</h1>
-		<p><a href="update/${item.code}">수정</a></p>
-		<p><fmt:formatDate value="${item.regDate}" pattern="yyyy년 mm월 dd일"/></p>
-		<p>${item.maskname}</p>
-		<div>
-			<c:forEach items="${item.images}" var="image">
-				<img src="${image.fullpath}" alt="${image.filename}" />
-			</c:forEach>
+	<jsp:include page="/WEB-INF/views/common/header.jsp"></jsp:include>
+	<div class="review-form">
+		<h1 class="text-center pt-20 pb-10 px-40 font-weight-bold">리뷰</h1>
+		<div class="flex justify-between item-center pt-10 pb-50 px-40">
+			<h2 class="font-size-large font-weight-normal">추천과자 랜덤 구독 서비스</h2>
+			<span class="font-size-large font-weight-bold font-red"><fmt:formatNumber pattern="###,###,###원" value="${item.price}"/></span>
 		</div>
-		<h3>${item.contents}</h3>
-		<p>별점 : ${item.rating}</p>
+		<div class="input-wrapper">
+			<div class="flex item-center py-10">
+				<p class="pr-10 font-size-medium font-weight-normal">별점</p>
+				<div class="rating flex justify-center" data-rating="${item.rating}"></div>
+			</div>
+			<div class="py-15">
+				<p class="review-item-contents">${item.contents}</p>
+			</div>
+			<div class="text-right">
+				<a class="btn small round gray" href="list">돌아가기</a>
+			</div>
+		</div>
 	</div>
+	<jsp:include page="/WEB-INF/views/common/footer.jsp"></jsp:include>
 </body>
 </html>
--- src/main/webapp/resources/js/review_rating.js
+++ src/main/webapp/resources/js/review_rating.js
@@ -1,39 +1,54 @@
 $(function () {
+	// 리뷰 리스트에서 사용
 	// 평점(별점)을 표시해야하는 요소를 찾는다
 	const rating = $('.review-item .rating');
 	// 찾아서 data-rating 만큼 별을 표시한다
 	for (r of rating) {
 		const ratingValue = $(r).data('rating');
-		const floor = Math.floor(ratingValue);
-		const ceil = Math.ceil(ratingValue);
-		for (let i = 1; i <= floor; i++) {
-			const elem = createStar('star');
-			$(r).append(elem);
-		}
-		// 0.5는 반개 표시한다
-		if(ratingValue - floor !== 0){
-			$(r).append(createStar('half'));
-		}
-
-		for(let i = ceil; i < 5; i++){
-			$(r).append(createStar('empty'));
-		}
+		initRatingWrapper($(r), ratingValue, false);
 	}
 
-	function createStar(type) {
-		const elem = $('<span>');
-		switch (type) {
-			case 'star':
-				elem.addClass('star');
-				break;
-			case 'half':
-				elem.addClass('star-half');
-				break;
-			case 'empty':
-				elem.addClass('star-empty');
-				break;
-		}
+	// 리뷰 상세에서 사용
+	const reviewDetailRating = $('.review-form .rating');
+	const itemRating = reviewDetailRating.data('rating')
 
-		return elem;
-	}
+	initRatingWrapper(reviewDetailRating, itemRating, true);
 });
+
+function initRatingWrapper(wrapper, ratingValue, big){
+	const floor = Math.floor(ratingValue);
+	const ceil = Math.ceil(ratingValue);
+	for (let i = 1; i <= floor; i++) {
+		const elem = createStar('star', big);
+		wrapper.append(elem);
+	}
+	// 0.5는 반개 표시한다
+	if(ratingValue - floor !== 0){
+		wrapper.append(createStar('half', big));
+	}
+
+	for(let i = ceil; i < 5; i++){
+		wrapper.append(createStar('empty', big));
+	}
+}
+
+function createStar(type, big) {
+	const elem = $('<span>');
+	switch (type) {
+		case 'star':
+			elem.addClass('star');
+			break;
+		case 'half':
+			elem.addClass('star-half');
+			break;
+		case 'empty':
+			elem.addClass('star-empty');
+			break;
+	}
+
+	if(big){
+		elem.addClass('big');
+	}
+
+	return elem;
+}
