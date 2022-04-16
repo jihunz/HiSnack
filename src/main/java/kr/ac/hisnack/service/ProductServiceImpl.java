a13f62b3fb6b6cf7d857fca0ba7b5088bd85d991
tivmzk <ost0108@naver.com>
2022-04-16 22:37:58 +0900

    feat : 장바구니 css, 백엔드 기능들 추가

--- src/main/java/kr/ac/hisnack/service/ProductServiceImpl.java
+++ src/main/java/kr/ac/hisnack/service/ProductServiceImpl.java
@@ -137,8 +137,10 @@ public class ProductServiceImpl implements ProductService {
 	public int priceTotal(List<OrderedProduct> products) {
 		int total = 0;
 		for(OrderedProduct product : products) {
-			Product item = dao.item(product.getPcode());
-			total += item.getPrice() * product.getAmount();
+			if(product.isChecked()) {
+				Product item = dao.item(product.getPcode());
+				total += item.getPrice() * product.getAmount();	
+			}
 		}
 		return total;
 	}
@@ -153,6 +155,7 @@ public class ProductServiceImpl implements ProductService {
 		for(OrderedProduct product : products) {
 			Product item = dao.item(product.getPcode());
 			item.setAmount(product.getAmount());
+			item.setChecked(product.isChecked());
 			list.add(item);
 		}
 		return list;
--- src/main/java/kr/ac/hisnack/service/ReviewServiceImpl.java
+++ src/main/java/kr/ac/hisnack/service/ReviewServiceImpl.java
@@ -1,6 +1,5 @@
 package kr.ac.hisnack.service;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
