0887cdd0626a3ac401e61399ff0709bc5c2eba5b
Jihunz <thespringsong@gmail.com>
2022-04-29 20:02:16 +0900

    chore: 제품 상세 페이지 주문하기 클릭 시 상품 추가되도록 변경

--- src/main/webapp/resources/js/cart_button.js
+++ src/main/webapp/resources/js/cart_button.js
@@ -1,25 +1,34 @@
 $(function(){
-	$('#cart-btn').click(function(){
-		const code = $('#item').data('code');
-		const amount = $('#item input[type="number"]').val();
+	$('#cart-btn').click(() => {
+		cart_ajax("cart");
+	});
 
-		const formData = new FormData();
-		formData.append('pcode', code);
-		formData.append('amount', amount);
-
-		$.ajax("/rest/cart", {
-			method:'post',
-			data:formData,
-			cache:false,
-			contentType:false,
-			processData:false,
-			success:function(result){
-				console.log(result.msg);
-				alert("상품이 추가되었습니다");
-			},
-			error:function(xhr, status, err){
-				console.log(`error ${xhr.status} : ${xhr.responseText}`);
-			}
-		});
+	$("a[href='/orders/cart'").click(() => {
+		cart_ajax();
 	});
 });
+
+function cart_ajax(type) {
+	const code = $('#item').data('code');
+	const amount = $('#item input[type="number"]').val();
+
+	const formData = new FormData();
+	formData.append('pcode', code);
+	formData.append('amount', amount);
+
+	$.ajax("/rest/cart", {
+		method:'post',
+		data: formData,
+		cache:false,
+		contentType:false,
+		processData:false,
+		success:function(result){
+			console.log(result.msg);
+			
+			if(type == "cart") alert("상품이 추가 되었습니다");
+		},
+		error:function(xhr, status, err){
+			console.log(`error ${xhr.status} : ${xhr.responseText}`);
+		}
+	});
+}
