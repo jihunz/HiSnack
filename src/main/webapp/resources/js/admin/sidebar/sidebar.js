107fa0cc484cc1c7e0ca706ad56832cfa9b6724a
Jihunz <thespringsong@gmail.com>
2022-05-09 19:55:47 +0900

    fix: 사이드바 스타일 수정

--- src/main/webapp/resources/js/admin/sidebar/sidebar.js
+++ src/main/webapp/resources/js/admin/sidebar/sidebar.js
@@ -1,6 +1,6 @@
 // 사이드바 컴포넌트 -> 메뉴를 삽입
 class Sidebar extends React.Component {
-    
+
     render() {
         const { onSetCategory } = this.props;
 
@@ -9,7 +9,7 @@ class Sidebar extends React.Component {
                 <div className="sidebar-item-logo-box">
                     <a href="/"><img src="re/img/logo2.svg" id="logo"></img></a>
                 </div>
-                <Menu 
+                <Menu
                     onSetCategory={onSetCategory}
                 />
             </div>
@@ -23,14 +23,14 @@ class Menu extends React.Component {
         const { onSetCategory } = this.props;
 
         return (
-            <div className="sidebar-items">
+            <>
                 <div className="sidebar-item-menu-box">
-                    <h4>MENU</h4>
+                    <h4 className="sidebar-ml">MENU</h4>
                 </div>
-                <MenuList 
+                <MenuList
                     onSetCategory={onSetCategory}
                 />
-            </div>
+            </>
 
         );
     }
@@ -42,14 +42,14 @@ class MenuList extends React.Component {
         const { onSetCategory } = this.props;
 
         return (
-            <div >
+            <div className="sidebar-item-menu-container sidebar-ml">
                 <ul className="sidebar-item-menu-ul">
-                    <li onClick={ () => onSetCategory("product", "제품") }>제품 관리</li>
-                    <li onClick={ () => onSetCategory("sub", "구독") }>구독 관리</li>
-                    <li onClick={ () => onSetCategory("orders", "주문") }>주문 관리</li>
-                    <li onClick={ () => onSetCategory("member", "회원") }>회원 관리</li>
-                    <li onClick={ () => onSetCategory("review", "리뷰") }>리뷰 관리</li>
-                    <li onClick={ () => onSetCategory("tag", "태그") }>태그 관리</li>
+                    <li onClick={() => onSetCategory("product", "제품")}>제품 관리</li>
+                    <li onClick={() => onSetCategory("sub", "구독")}>구독 관리</li>
+                    <li onClick={() => onSetCategory("orders", "주문")}>주문 관리</li>
+                    <li onClick={() => onSetCategory("member", "회원")}>회원 관리</li>
+                    <li onClick={() => onSetCategory("review", "리뷰")}>리뷰 관리</li>
+                    <li onClick={() => onSetCategory("tag", "태그")}>태그 관리</li>
                 </ul>
             </div>
         );
