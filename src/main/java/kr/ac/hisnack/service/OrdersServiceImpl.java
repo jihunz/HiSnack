457f03979554db30bf161048bf24ba0ccc2e11cf
tivmzk <ost0108@naver.com>
2022-05-14 16:24:31 +0900

    feat : 스케쥴러로 구독 자동 갱신 추가

--- src/main/java/kr/ac/hisnack/service/OrdersServiceImpl.java
+++ src/main/java/kr/ac/hisnack/service/OrdersServiceImpl.java
@@ -9,8 +9,11 @@ import org.springframework.transaction.annotation.Transactional;
 import kr.ac.hisnack.dao.OrderedProductDao;
 import kr.ac.hisnack.dao.OrdersDao;
 import kr.ac.hisnack.dao.ProductDao;
+import kr.ac.hisnack.model.MailHtmlTemplate;
+import kr.ac.hisnack.model.Member;
 import kr.ac.hisnack.model.OrderedProduct;
 import kr.ac.hisnack.model.Orders;
+import kr.ac.hisnack.util.EmailSender;
 import kr.ac.hisnack.util.Pager;
 
 @Service
@@ -25,6 +28,10 @@ public class OrdersServiceImpl implements OrdersService {
 	ProductService ps;
 	@Autowired
 	MemberTagService mts;
+	@Autowired
+	MemberService ms;
+	@Autowired
+	EmailSender mailSender;
 	
 /**
  * 주문 추가
@@ -120,5 +127,28 @@ public class OrdersServiceImpl implements OrdersService {
 		List<Orders> list = list(pager);
 		
 		return list.size() > 0 ? list.get(0) : null;
+	}
+
+	@Override
+	public void subscribe(Orders item, Member user, String subject, String ... contents) {
+//		여기서 추천 범위를 설정할 수 있다
+		List<OrderedProduct> list = ps.recommend(item.getId(), 30, item.getTotal(), (int)(item.getTotal() * 0.2));
+		item.setProducts(list);
+		
+		add(item);
+		
+		if(user == null) user = ms.item(item.getId());
+		
+//		구독을 하면 이메일을 보낸다
+		if(user.getEmail() != null && !user.getEmail().equals("")) {
+//			이메일을 위한 html 인스턴스를 사용
+	        MailHtmlTemplate html = new MailHtmlTemplate("html/subscribe_mail_template.html");
+//	        내용 입력
+	        html.setContents(contents);
+//	        이미지로 들어갈 상품 설정
+	        html.setOrderedProduct(item(item.getCode()).getProducts());
+//	        이미지가 포함된 html로 메일 보내기
+	        mailSender.sendHtmlWithImageEmail(user.getEmail(), user.getName(), subject, html);
+		}
 	}
 }
