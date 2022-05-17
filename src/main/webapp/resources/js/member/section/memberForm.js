ec7a014c98cb3b251475b8fa173f92e37df27ceb
jihunz <thespringsong@gmail.com>
2022-05-17 10:31:30 +0900

    회원 정보 수정 폼에 주소 찾기 추가

--- src/main/webapp/resources/js/member/section/memberForm.js
+++ src/main/webapp/resources/js/member/section/memberForm.js
@@ -9,19 +9,20 @@ class MemberForm extends React.Component {
                     <h1>회원 정보 수정</h1>
                     <div>
                         <label>이름</label>
-                        <input name="name" value={item.name} onChange={onMemberChange} />
+                        <input type="text" name="name" value={item.name} onChange={onMemberChange} />
                     </div>
                     <div>
                         <label>주소</label>
-                        <input name="address" value={item.address != null ? item.address : null} onChange={onMemberChange} />
+                        <input type="text" id="address" name="address" value={item.address != null ? item.address : ''} onChange={onMemberChange} />
+                        <button type="button" onClick={execDaumPostcode} className="addressbtn text-center">주소 찾기</button>
                     </div>
                     <div>
                         <label>전화번호</label>
-                        <input name="tel" value={item.tel != null ? item.tel : null} onChange={onMemberChange} />
+                        <input type="text" id="tel" name="tel" value={item.tel != null ? item.tel : ''} onChange={onMemberChange} />
                     </div>
                     <div>
                         <label>이메일</label>
-                        <input name="email" value={item.email} onChange={onMemberChange} />
+                        <input type="text" name="email" value={item.email} onChange={onMemberChange} />
                     </div>
                     <button type="button" onClick={() => onUpdate("member")}>수정</button>
                 </form>
